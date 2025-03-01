fc9e420c47841ee8add6ca75ee52f0ab67b58474
AbhishekYadav76 <abhishekyadav456@gamil.com>
2025-03-01 10:10:17 +0530

    Added from Access Modifiers

---
+++ Accessmodifier/PrivateAcessModifier.java
@@ -0,0 +1,20 @@
+
+
+class PrivateEx{
+   private int secretcode=1245;
+
+   void showcode(){
+    System.out.println(secretcode);
+   }
+}
+
+
+public class PrivateAcessModifier{
+    public static void main(String[] args) {
+        
+        System.out.println(secretcode);
+        PrivateEx obj = new PrivateEx();
+        obj.showcode();
+        
+    }
+}
