37d366096527d5d6058c86442bd92593f3515cea
AbhishekYadav76 <abhishekyadav456@gamil.com>
2025-03-05 15:40:43 +0530

    Mock Java ClassTest

---
+++ Mock Java Test/Coding Part Java/SumOfDigitUsingRecursion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class SumOfDigitRecursion{
+
+    static int sumOfDigit(int num)
+    {
+        if(num==0) 
+          return 0;
+        else 
+        return num%10 + sumOfDigit(num/10);
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+        System.out.println(sumOfDigit(num));
+    }
+}
